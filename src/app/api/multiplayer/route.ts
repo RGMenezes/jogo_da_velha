6fc97afbaf3cf5b6f53b8f59406cc01532ba69ae
rgmenezes <faelgmp@gmail.com>
2023-12-19 13:33:28 -0300

    SSE response funcionando

--- src/app/api/multiplayer/route.ts
+++ src/app/api/multiplayer/route.ts
@@ -1,8 +1,34 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 
+let messageGlobal: string;
 
-export async function GET( req: typeof NextRequest) {
-  const res: typeof NextResponse = NextResponse
+export async function GET( req: Request) {
+  const { readable, writable } = new TransformStream()
+
+  const sendEvent = (data: Record<string, any>) => {
+    writable.getWriter().write(`data: ${JSON.stringify(data)}\n\n`)
+  }
+
+  setTimeout(() => {
+    sendEvent({ message: `testando ${messageGlobal}` })
+  }, 10000)
   
-  return res.json({message: 'Testando (SSE)'})
+  const res =  new Response(readable, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
+    }
+  })
+
+  return res
+}
+
+export async function POST(req: Request) {
+  const { message }: {message: string} = await req.json()
+  
+  messageGlobal = message
+
+  return NextResponse.json("ação no servidor")
 }
--- src/app/search_player/page.tsx
+++ src/app/search_player/page.tsx
@@ -8,6 +8,7 @@ import Button from '@form/Button'
 import { useSelector } from 'react-redux'
 import { User } from '@/redux/user/slice'
 import { useEffect } from 'react'
+import axios from '@/server/axios'
 
 export default function SearchPlayer(){
   const router: AppRouterInstance = useRouter()
@@ -15,12 +16,18 @@ export default function SearchPlayer(){
 
   const user = useSelector((state: {userReducer: User}) => state.userReducer)
 
+  function testeSSE(){
+    axios.post('multiplayer', {message: 'qualquer coisa'}).then((res) => {
+      console.log(`sucesso na conexão: ${res.data}`)
+    }).catch(err => console.log(`erro na conexão: ${err}`))
+  }
+
   useEffect(() => {
     const eventSource = new EventSource('/api/multiplayer')
 
-    eventSource.onmessage = (e) => {
-      const data = JSON.parse(e.data)
-      console.log(data)
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      console.log(data.message)
     }
   }, [])
 
@@ -28,7 +35,7 @@ export default function SearchPlayer(){
     <section className={styles.container}>
       <h1>Lista de jogadores online</h1>
 
-      <div>
+      <div onClick={testeSSE}>
         <h2>Info users</h2>
         <p>nome: {user.userName}</p>
         <p>email: {user.email}</p>
