51614537efc05e5962c407b9725189bd7fa9ca17
rgmenezes <faelgmp@gmail.com>
2024-01-02 20:01:29 -0300

    comentando codigo

--- src/app/api/multiplayer/route.ts
+++ src/app/api/multiplayer/route.ts
@@ -3,30 +3,30 @@ import LogedUser from "@/server/model/LogedUser"
 
 export async function GET( req: Request ) {
 
-  async function sendEvent(data: Record<string, any>, writable: WritableStream<any>) {
-    const writer = writable.getWriter()
+  // async function sendEvent(data: Record<string, any>, writable: WritableStream<any>) {
+  //   const writer = writable.getWriter()
 
-    await writer.write(`data: ${JSON.stringify(data)}\n\n`).then().catch((err) => {
-      writer.abort()
-    }).finally(() => writer.releaseLock())
-  }
+  //   await writer.write(`data: ${JSON.stringify(data)}\n\n`).then().catch((err) => {
+  //     writer.abort()
+  //   }).finally(() => writer.releaseLock())
+  // }
 
-  async function updateListLogedUser(writable: WritableStream<any>){
-    await LogedUser.find({}).then((res) => {
-      sendEvent({logedUsers: res}, writable)
-    }).catch(err => console.log(err))
-  }
+  // async function updateListLogedUser(writable: WritableStream<any>){
+  //   await LogedUser.find({}).then((res) => {
+  //     sendEvent({logedUsers: res}, writable)
+  //   }).catch(err => console.log(err))
+  // }
 
   try {
     await Database()
     if(!LogedUser) throw new Error('Erro ao conectar-se com a collection')
     const { readable, writable } = new TransformStream()
   
-    const changeStream = LogedUser.watch()
+    // const changeStream = LogedUser.watch()
   
-    changeStream.on('change', (change) => updateListLogedUser(writable))
+    // changeStream.on('change', (change) => updateListLogedUser(writable))
   
-    await updateListLogedUser(writable)
+    // await updateListLogedUser(writable)
     
     return new Response(readable, {
       status: 200,
@@ -39,7 +39,7 @@ export async function GET( req: Request ) {
   } catch (err) {
     console.error(err)
     return new Response(`Error: ${err}`, {
-      status: 200,
+      status: 404,
       headers: {
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
